a4c03df0aca1e8dc0d9daed49f37d20e0da6aeda
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-05-10 10:50:31 +0300

    test

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -3,9 +3,14 @@ package ru.netology;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CallbackTest {
@@ -13,25 +18,42 @@ public class CallbackTest {
 
     static void setUpAll() {
 // убедитесь, что файл chromedriver.exe расположен именно в каталоге C:\tmp
-        System.setProperty("webdriver.chrome.driver", "C:\\tmp\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
+
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
     }
+
     @AfterEach
     void tearDown() {
         driver.quit();
         driver = null;
     }
-    @Test
-    void shouldTestV1() {
-        driver.get("http://localhost:9999/");
-        driver.findElement()
-    }
 
     @Test
-    void shouldTestSomething() {
-        throw new UnsupportedOperationException();
+    void shouldTestV1() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+       Thread.sleep(8000);
+        List<WebElement> imputs = driver.findElements(By.tagName("imput"));
+        imputs.get(0).sendKeys("Антонов Антон");
+        imputs.get(1).sendKeys("+79995555555");
+        driver.findElement(By.className("checkbox__box"));
+        driver.findElement(By.className("button__text"));
+        // driver.findElement().sendKeys("+79995555555");
+        // driver.findElement().click();
+        //  driver.findElement().click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("order-success")).getText();
+
+        assertEquals(expected, actual);
+
     }
+
+    //@Test
+    // void shouldTestSomething() {
+    //  throw new UnsupportedOperationException();
+    // }
 }
