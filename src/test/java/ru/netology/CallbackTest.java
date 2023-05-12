9120572bf5cbf8a0c38051b4b55f6e9ffb090223
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-05-12 15:46:20 +0300

    test

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,12 +1,15 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -15,45 +18,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
     private WebDriver driver;
-
+@BeforeAll
     static void setUpAll() {
 // убедитесь, что файл chromedriver.exe расположен именно в каталоге C:\tmp
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
+
+
+        @AfterEach
+        void teardown() {
+            driver.quit();
+            driver = null;
+        }
 
     @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @Test
     void shouldTestV1() throws InterruptedException {
         driver.get("http://localhost:9999/");
-       Thread.sleep(8000);
-        List<WebElement> imputs = driver.findElements(By.tagName("imput"));
-        imputs.get(0).sendKeys("Антонов Антон");
-        imputs.get(1).sendKeys("+79995555555");
-        driver.findElement(By.className("checkbox__box"));
-        driver.findElement(By.className("button__text"));
-        // driver.findElement().sendKeys("+79995555555");
-        // driver.findElement().click();
-        //  driver.findElement().click();
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Антонов Антон");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7999555555");
+
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+
+        driver.findElement(By.className("button__text")).click();
 
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.className("order-success")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
 
         assertEquals(expected, actual);
 
     }
 
-    //@Test
-    // void shouldTestSomething() {
-    //  throw new UnsupportedOperationException();
-    // }
+
 }
