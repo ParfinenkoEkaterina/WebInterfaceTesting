00f83f57f97afbc6efccbfa5f4c7de1dcc797364
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-05-13 15:08:10 +0300

    tests passed

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -43,7 +43,7 @@ public class CallbackTest {
     void shouldTestV1() throws InterruptedException {
         driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Антонов Антон");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7999555555");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79995555555");
 
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
 
@@ -53,7 +53,88 @@ public class CallbackTest {
         String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
 
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
+
+    }
+   @Test
+    void shouldTestInvalidName() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Antonov Anton");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7999555555");
+
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+
+        driver.findElement(By.className("button__text")).click();
+
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldTestInvalidPhone() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Антонов Антон");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+799955555");
+
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+
+        driver.findElement(By.className("button__text")).click();
+
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldTestInvalidAgreement() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Антонов Антон");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79995555555");
+        driver.findElement(By.className("button__text")).click();
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid .checkbox__text")).getText();
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldTestInvalidNoName() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+      //  driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Антонов Антон");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79995555555");
+
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+
+        driver.findElement(By.className("button__text")).click();
+
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        String expected = "Поле обязательно для заполнения";
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldTestInvalidNoPhone() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Антонов Антон");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button__text")).click();
+
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        String expected = "Поле обязательно для заполнения";
+
+        assertEquals(expected, actual);
 
     }
 
