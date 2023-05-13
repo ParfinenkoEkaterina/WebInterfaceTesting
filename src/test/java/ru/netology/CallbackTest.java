a87595b39062e21cfdf9ad17bae6eca73a4deca7
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-05-13 13:00:12 +0300

    driver added

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,10 +1,7 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,14 +11,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CallbackTest {
-    WebDriver driver;
+    WebDriver driver = new ChromeDriver();
+
      @BeforeAll
     static void setUpAll() {
 // убедитесь, что файл chromedriver.exe расположен именно в каталоге C:\tmp
         WebDriverManager.chromedriver().setup();
+
     }
 
         @AfterEach
@@ -54,9 +53,8 @@ public class CallbackTest {
         String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
 
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
     }
-
 
 }
